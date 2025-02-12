57cbc77b4b5d6332a7492f1ead2c6b5b6c504096
GCYYfun <734536637@qq.com>
2025-02-12 22:11:50 +0800

    fix: fix some written bug

--- FancyWorld/server.py
+++ FancyWorld/server.py
@@ -1,11 +1,22 @@
+import time
 from typing import List, Union
 from fastapi.responses import StreamingResponse
 from fastapi import FastAPI
 
 from mlong.model import Model
 from mlong.utils import user, system, assistant
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+# 定义请求数据模型
+class ChatParam(BaseModel):
+    model_id: str
+    messages: List[dict]
+    temperature: float = 0
+    maxTokens: int = 100
+    stream: bool = False
 
 
 @app.get("/")
@@ -13,26 +24,69 @@ def root():
     return {"Hello": "World"}
 
 
-@app.get(f"models")
+@app.get(f"/models")
 def models():
     return {"models": ["us.anthropic.claude-3-5-sonnet-20241022-v2:0"]}
 
 
-@app.get("/chat")
-def chat(
-    model_id: str = None,
-    messages: List = None,
-    temperature: float = 0,
-    maxTokens: int = 100,
-):
+async def parse_response(response):
+    # 将输入的提示按句子分割
+    s = response.stream
+    if s:
+        for event in s:
+            match event:
+                case {"messageStart": {"role": "assistant"}}:
+                    yield f"{event['messageStart']['role']}:\n"
+                case {"contentBlockDelta": {"delta": {"text": text}}}:
+                    yield text
+                # case {"messageStop": {"stopReason": reason}}:
+                #     yield f"\nStop reason: {reason}\n"
+                # case {"metadata": metadata}:
+                #     if "usage" in metadata:
+                #         usage = metadata["usage"]
+                #         yield f"Token usage\n"
+                #         yield f"Input tokens: {usage['inputTokens']}\n"
+                #         yield f"Output tokens: {usage['outputTokens']}\n"
+                #         yield f"Total tokens: {usage['totalTokens']}\n"
+                #     if "metrics" in metadata:
+                #         metrics = metadata["metrics"]
+                #         yield f"Latency: {metrics['latencyMs']} milliseconds\n"
+
+
+@app.post("/chat")
+def chat(param: ChatParam):
     client = Model()
-    response = client.chat(
-        model_id=model_id,  # "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
-        messages=messages,
-        temperature=0.5,
-        maxTokens=maxTokens,
-    )
-    return {"result": response.choices[0].message.content}
+    if param.stream:
+        response = client.chat(
+            model_id=param.model_id,
+            messages=param.messages,
+            temperature=param.temperature,
+            maxTokens=param.maxTokens,
+            stream=param.stream,
+        )
+
+        async def event_generator():
+            async for item in parse_response(response):
+                yield item.encode("utf-8")  # 返回字节流
+
+        # 返回流式结果
+        return StreamingResponse(
+            event_generator(),
+            media_type="text/event-stream",
+            headers={
+                "Cache-Control": "no-cache",  # 禁用缓存
+                "Connection": "keep-alive",  # 保持连接
+                "X-Accel-Buffering": "no",  # 禁用代理缓冲
+            },
+        )
+    else:
+        response = client.chat(
+            model_id=param.model_id,  # "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
+            messages=param.messages,
+            temperature=param.temperature,
+            maxTokens=param.maxTokens,
+        )
+        return {"result": response.choices[0].message.content}
 
 
 @app.get("/agent")
